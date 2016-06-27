b3a96185af284e65a2c0c30e0daff31e1ffd3ba7
Tomohiko Mimura <mito.5525@gmail.com>
2016-06-27 09:50:19 +0900

    学籍番号にUNIQUE制約を付与する

--- priv/repo/migrations/20160625152221_initial_schema.exs
+++ priv/repo/migrations/20160625152221_initial_schema.exs
@@ -4,12 +4,13 @@ defmodule Cuenta.Repo.Migrations.InitialSchema do
   def change do
     create table(:users) do
       add :name, :string, null: false
-      add :number, :string, size: 9, null: false
+      add :number, :string, size: 9, null: false, unique: true
       add :encrypted_password, :string, null: false
       add :college_id, :integer, null: false
 
       timestamps()
     end
 
+    create unique_index(:users, [:number], name: :unique_number)
   end
 end
