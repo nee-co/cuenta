29adcf85dfa57a68046b230bd12ae1fab7fd52a6
Tomohiko Mimura <mito.5525@gmail.com>
2016-07-02 01:46:34 +0900

    JSONレスポンス内容を調整する
    - パスワードは返さない

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -10,10 +10,9 @@ defmodule Cuenta.UserView do
   end
 
   def render("user.json", %{user: user}) do
-    %{id: user.id,
+    %{user_id: user.id,
       name: user.name,
       number: user.number,
-      crypted_password: user.crypted_password,
       college_id: user.college_id}
   end
 end
