fdad2b85ecf6aa0fb2a6a0cb8a99fd4ca32dd175
Tomohiko Mimura <mito.5525@gmail.com>
2016-07-02 02:12:46 +0900

    カラム名修正し忘れ

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -2,7 +2,7 @@ defmodule Cuenta.UserControllerTest do
   use Cuenta.ConnCase
 
   alias Cuenta.User
-  @valid_attrs %{college_id: 42, crypted_password: "some content", name: "some content", number: "some content"}
+  @valid_attrs %{college_id: 42, encrypted_password: "some content", name: "some content", number: "some content"}
   @invalid_attrs %{}
 
   setup %{conn: conn} do
