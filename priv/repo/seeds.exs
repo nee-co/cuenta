de0e937950a4442ce4d7def4d0feb74a156fd65a
Tomohiko Mimura <mito.5525@gmail.com>
2016-07-08 00:53:44 +0900

    カレッジのSeedを追加する
    $ mix run priv/repo/seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+#TODO: 環境ごとにseedを分ける
+if Cuenta.College |> Cuenta.Repo.all == [] do
+  Cuenta.Repo.insert!(%Cuenta.College{id: 1, name: "クリエイター", code: "A"})
+  Cuenta.Repo.insert!(%Cuenta.College{id: 2, name: "ミュージック", code: "B"})
+  Cuenta.Repo.insert!(%Cuenta.College{id: 3, name: "IT", code: "C"})
+  Cuenta.Repo.insert!(%Cuenta.College{id: 4, name: "テクノロジー", code: "D"})
+  Cuenta.Repo.insert!(%Cuenta.College{id: 5, name: "医療・保育", code: "E"})
+  Cuenta.Repo.insert!(%Cuenta.College{id: 6, name: "スポーツ", code: "F"})
+  Cuenta.Repo.insert!(%Cuenta.College{id: 7, name: "デザイン", code: "G"})
+end
