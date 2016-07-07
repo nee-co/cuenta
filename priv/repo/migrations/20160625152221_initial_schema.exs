da5a7fa5b340c7da84ba6bb60b62b0764dc3a061
Tomohiko Mimura <mito.5525@gmail.com>
2016-07-07 23:12:40 +0900

    カレッジテーブルを追加する

--- priv/repo/migrations/20160625152221_initial_schema.exs
+++ priv/repo/migrations/20160625152221_initial_schema.exs
@@ -2,15 +2,21 @@ defmodule Cuenta.Repo.Migrations.InitialSchema do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:colleges) do
       add :name, :string, null: false
-      add :number, :string, size: 9, null: false, unique: true
-      add :encrypted_password, :string, null: false
-      add :college_id, :integer, null: false
+      add :code, :string, size: 1,  null: false, unique: true
 
       timestamps()
     end
 
+    create table(:users) do
+      add :name, :string, null: false
+      add :number, :string, size: 9, null: false, unique: true
+      add :encrypted_password, :string, null: false
+      add :college_id, references(:colleges, on_delete: :nilify_all, on_update: :update_all)
+
+      timestamps()
+    end
     create unique_index(:users, [:number], name: :unique_number)
   end
 end
