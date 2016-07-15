899c9d5dff9701502403512458abde32feaa4792
Tomohiko Mimura <mito.5525@gmail.com>
2016-07-15 22:19:19 +0900

    開発用初期データとして700人ユーザを投入する

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -20,3 +20,18 @@ if Cuenta.College |> Cuenta.Repo.all == [] do
   Cuenta.Repo.insert!(%Cuenta.College{id: 6, name: "スポーツ", code: "F"})
   Cuenta.Repo.insert!(%Cuenta.College{id: 7, name: "デザイン", code: "G"})
 end
+
+import Faker.Name.Ja, only: [name: 0]
+import Enum, only: [random: 1]
+
+insert_user = fn (num) ->
+  college_id = num + 1
+  college_code = ~W/A B C D E F G/ |> Enum.at(num)
+  year = random(0..20) |> Integer.to_string |> String.rjust(2, ?0)
+  personal = random(0..9999) |> Integer.to_string |> String.rjust(4, ?0)
+  number = "G0#{year}#{college_code}#{personal}"
+
+  Cuenta.Repo.insert!(%Cuenta.User{name: name, number: number, college_id: college_id})
+end
+
+1..700 |> Enum.to_list |> Enum.each(&insert_user.(rem(&1, 7)))
