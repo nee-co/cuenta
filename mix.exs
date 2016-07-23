915c2dda4d3cc745ea4a5b29eb425d25a7598128
Tomohiko Mimura <mito.5525@gmail.com>
2016-07-23 20:49:16 +0900

    add comeonin [Password hashing library]

--- mix.exs
+++ mix.exs
@@ -18,8 +18,8 @@ defmodule Cuenta.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Cuenta, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :mariaex]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger,
+                    :gettext, :phoenix_ecto, :mariaex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,6 +38,7 @@ defmodule Cuenta.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 2.5"},
      {:faker, "~> 0.5", only: [:test, :dev]}]
   end
 
