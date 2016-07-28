d0b3843d86616bcb97f066e9b09027bf9718f879
Tomohiko Mimura <mito.5525@gmail.com>
2016-07-28 15:55:58 +0900

    各設定を環境変数を参照するようにする

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,13 +9,13 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or you later on).
 config :cuenta, Cuenta.Endpoint,
-  secret_key_base: "IdH0eUmgr3/E0HEyOfNGSNltZgCMpcazXUr5o8yFTUSWX3sIfJ852tSTh2ZHKTVR"
+  secret_key_base: "IdH0eUmgr3/E0HEyOfNGSNltZgCMpcazXUr5o8yFTUSWX3sIfJ852tSTh2ZHKTVR" # TODO: これも環境変数にする
 
 # Configure your database
 config :cuenta, Cuenta.Repo,
   adapter: Ecto.Adapters.MySQL,
-  username: "root",
-  password: "",
+  username: System.get_env("CUENTA_DATABASE_USER"),
+  password: System.get_env("CUENTA_DATABASE_PASSWORD"),
   database: "cuenta_prod",
-  hostname: "db", # docker? "db" : "localhost"
+  hostname: System.get_env("CUENTA_DATABASE_HOST"),
   pool_size: 20
