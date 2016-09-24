c31f40d3f6e129e592755523cf41f3bd618987e0
Tomohiko Mimura <mito.5525@gmail.com>
2016-09-25 01:49:58 +0900

    少し楽にヘッダー取得できた

--- lib/cuenta/plugs/require_login.ex
+++ lib/cuenta/plugs/require_login.ex
@@ -7,10 +7,10 @@ defmodule Cuenta.Plug.RequireLogin do
   def init(default), do: default
 
   def call(conn, _) do
-    case conn.req_headers |> Enum.find(&elem(&1, 0) == "x-consumer-custom-id") do
-      { "x-consumer-custom-id", user_id } ->
+    case get_req_header(conn, "x-consumer-custom-id") do
+      [user_id | _] ->
         assign(conn, :current_user, Repo.get!(User, user_id) |> Repo.preload(:college))
-      _ -> send_resp(conn, 401, "")
+      _ -> send_resp(conn, 401, "") |> halt
     end
   end
 end
