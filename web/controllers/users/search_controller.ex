7ff1632d4485493519db91c4edda94e7eaacb9af
Tomohiko Mimura <mito.5525@gmail.com>
2016-11-08 00:03:53 +0900

    共通処理をまとめた

--- web/controllers/users/search_controller.ex
+++ web/controllers/users/search_controller.ex
@@ -4,47 +4,33 @@ defmodule Cuenta.UserSearchController do
   alias Cuenta.User
 
   def search(conn, %{"str" => str, "user_ids" => user_ids, "college_codes" => _college_codes}) do
-    users = User
-    |> User.in_user(~i/#{user_ids}/)
-    |> User.like_name_or_number(str)
-    |> limit(50)
-    |> Repo.all |> Repo.preload(:college)
-
-    render(conn, "search.json", users: users)
+    base_query(str) |> User.in_user(~i/#{user_ids}/) |> base_render(conn)
   rescue
     ArgumentError -> send_resp(conn, 400, "")
   end
 
   def search(conn, %{"str" => str, "user_ids" => user_ids}) do
-    users = User
-    |> User.in_user(~i/#{user_ids}/)
-    |> User.like_name_or_number(str)
-    |> limit(50)
-    |> Repo.all |> Repo.preload(:college)
-
-    render(conn, "search.json", users: users)
+    base_query(str) |> User.in_user(~i/#{user_ids}/) |> base_render(conn)
   rescue
     ArgumentError -> send_resp(conn, 400, "")
   end
 
   def search(conn, %{"str" => str, "college_codes" => college_codes}) do
-    users = User
-    |> User.like_name_or_number(str)
-    |> User.in_college(~w/#{String.downcase(college_codes)}/)
-    |> limit(50)
-    |> Repo.all |> Repo.preload(:college)
-
-    render(conn, "search.json", users: users)
+    base_query(str) |> User.in_college(~w/#{String.downcase(college_codes)}/) |> base_render(conn)
   rescue
     ArgumentError -> send_resp(conn, 400, "")
   end
 
   def search(conn, %{"str" => str}) do
-    users = User
-    |> User.like_name_or_number(str)
-    |> limit(50)
-    |> Repo.all |> Repo.preload(:college)
+    base_query(str) |> base_render(conn)
+  end
 
+  defp base_query(str) do
+    User |> User.like_name_or_number(str) |> limit(50)
+  end
+
+  defp base_render(query, conn) do
+    users = query |> Repo.all |> Repo.preload(:college)
     render(conn, "search.json", users: users)
   end
 end
